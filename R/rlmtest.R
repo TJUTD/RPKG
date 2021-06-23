8be8d5008d9978bbc05dc1bbe05525c44c901a27
unknown <Tian.Jiang@utdallas.edu>
2021-06-23 14:57:21 -0500

    update

--- R/rlmtest.R
+++ R/rlmtest.R
@@ -1,19 +1,128 @@
-#' Title
+#' Robust Lagrange multiplier test for detecting ARCH/GARCH effect
 #'
-#' @param y
-#' @param x
-#' @param a.order
-#' @param sig.level
-#' @param crit.type
-#' @param nonp.method
-#' @param num.resamp
-#' @param num.discard
-#' @param CPP
+#' The function performs two resampling techniques to find critical values of the LM test, namely permutation and bootstrap.
+#'
+#' @param y  A numeric time series.
+#' @param x  A time series of regressors.
+#' @param a.order Order of the autoregressive ls.fit which must be a nonnegative integer number.
+#' @param crit.type A string parameter allowing to choose "asymptotic" or "nonparametric" options.
+#' @param nonp.method A string parameter allowing to choose "bootstrap" or "permutation" method of resampling
+#' "bootstrap" - resampling of the estimated residuals (with replacement).
+#' "permutation" - resampling of the estimated residuals (without replacement).
+#' @param num.resamp Number of bootstrap replications if \code{"crit.type"}="nonparametric". Default number is 1,000.
 #'
 #' @return
 #' @export
 #'
 #' @examples
+#'
+#' 1. ARCH effect
+#' # model y_t = y_tb + e_t
+#' # e_t ~ ARCH(1) coef
+#' a.0 <- 0.6
+#' a.1 <- 0.4
+#' t-distribution
+#' # z ~ t(5)        var = 5/(5-2)
+#' n <- 500
+#' z <- rt(n, df = 5)
+#' e <- numeric(n)
+#' e[1] <- rt(1, df = 5) * sqrt(a.0/(1.0-a.1) / (5/(5-2)))
+#' # Generate ARCH(1) process
+#' for(t in 2:n) {
+#'   e[t] <- z[t]*sqrt(a.0+a.1*e[t-1]^2)
+#' }
+#' x <- runif(n,0,10)
+#' y <- 0.25 + 0.5*x + e
+#'
+#' 2. independent series
+#' # e_t ~ t(5) no ARCH effect
+#' e <- rt(n, df = 5) * sqrt(a.0 / (5/(5-2)))
+#' x <- runif(n,0,10)
+#' y <- 0.25 + 0.5*x + e
+#'
+#' 3. real data
+#' #' library(rbenchmark)
+#' benchmark(rlmtest(y,x,crit.type = "nonparametric",nonp.method = "bootstrap",CPP = T),rlmtest(y,x,crit.type = "nonparametric",nonp.method = "bootstrap",CPP = F))
+#'
+#' data(GoldBTC)
+#' x <- GoldBTC$Gold
+#' y <- GoldBTC$BTC
+#'
+#' # test ARCH in Gold
+#' rlmtest(y,x,crit.type = "nonparametric",nonp.method = "bootstrap",CPP = T)
+#'
+#' # test ARCH in bitcoin
+#' rlmtest(y,x,crit.type = "nonparametric",nonp.method = "bootstrap",CPP = T)
+#'
+#' rlmtest(y,x,crit.type = "nonparametric",nonp.method = "bootstrap",CPP = T)
+#'
+# $`stat`
+# [1] 0.6353227
+#
+# $crit.val
+# 95%
+# 3.109865
+#
+# $p.value
+# [1] 0.262
+#
+# > rlmtest(x,y,crit.type = "nonparametric",nonp.method = "bootstrap",CPP = T)
+# $`stat`
+# [1] 41.62449
+#
+# $crit.val
+# 95%
+# 2.006489
+#
+# $p.value
+# [1] 0.001
+#
+# > rlmtest(x,crit.type = "nonparametric",nonp.method = "bootstrap",CPP = T)
+# $`stat`
+# [1] 32.4635
+#
+# $crit.val
+# 95%
+# 2.469525
+#
+# $p.value
+# [1] 0.001
+#
+# > rlmtest(y,crit.type = "nonparametric",nonp.method = "bootstrap",CPP = T)
+# $`stat`
+# [1] 0.6009142
+#
+# $crit.val
+# 95%
+# 2.672682
+#
+# $p.value
+# [1] 0.246
+#
+# > plot(1:361,x)
+# > plot(1:361,y)
+# > plot(1:361,x)
+# > plot(1:361,y)
+# > rlmtest(y,crit.type = "asymptotic",CPP = T)
+# $`stat`
+# [1] 0.6009142
+#
+# $crit.val
+# [1] 3.841459
+#
+# $p.value
+# [1] 0.4382294
+#
+# > rlmtest(x,crit.type = "asymptotic",CPP = T)
+# $`stat`
+# [1] 32.4635
+#
+# $crit.val
+# [1] 3.841459
+#
+# $p.value
+# [1] 1.214525e-08
+
 rlmtest <- function(y,
                     x = NA,
                     a.order = 1,
@@ -113,13 +222,13 @@ rlmtest <- function(y,
       res.sq.demean.bs <- res.sq.bs-mean(res.sq.bs)
 
       # run autoregressive of a.order on res.sq
-      if (CPP) {
-
+      if (1) {
+        # ar.fit.bs <- lm(res.sq.demean.bs[(a.order+1):n]~res.sq.demean.bs[1:(n-a.order)])
+        ar.fit.bs <- fLM(cbind(1,res.sq.demean.bs[1:(n-a.order)]), res.sq.demean.bs[(a.order+1):n])
       } else {
-
+        ar.fit.bs <- lm(res.sq.demean.bs[(a.order+1):n]~res.sq.demean.bs[1:(n-a.order)])
       }
-      # ar.fit.bs <- lm(res.sq.demean.bs[(a.order+1):n]~res.sq.demean.bs[1:(n-a.order)])
-      ar.fit.bs <- fLM(cbind(1,res.sq.demean.bs[1:(n-a.order)]), res.sq.demean.bs[(a.order+1):n])
+
       # AR(a.order) residuals
       ee.bs <- ar.fit.bs$residuals
 
@@ -140,26 +249,3 @@ rlmtest <- function(y,
   }
 }
 
-# library(RcppArmadillo)
-# Rcpp::sourceCpp("./src/Code.cpp")
-
-# if (!require(inline)) install.packages('inline')
-# library(inline)
-# src <- '
-# Rcpp::NumericMatrix Xr(Xs);
-# Rcpp::NumericVector yr(ys);
-# int n = Xr.nrow(), k = Xr.ncol();
-# arma::mat X(Xr.begin(), n, k, false);
-# arma::colvec y(yr.begin(), yr.size(), false);
-# int df = n - k;
-#
-# // fit model y ~ X, extract residuals
-# arma::colvec coef = arma::solve(X, y);
-# arma::colvec fit = X*coef;
-# arma::colvec res = y - X*coef;
-#
-# return Rcpp::List::create(Rcpp::Named("coefficients") = coef,
-# Rcpp::Named("fitted.values") = fit,
-# Rcpp::Named("residuals") = res);
-# '
-# fLm <- cxxfunction(signature(Xs="numeric", ys="numeric"), src, plugin="RcppArmadillo")
